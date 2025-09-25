a2f56e823c420b6811b8aa155d008a49d0bc5b9f
Wouter Meeder <woutermeeder67@gmail.com>
2025-09-25 10:09:08 +0200

    player wip

---
+++ 2509Project/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,51 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerController : MonoBehaviour
+{
+    public int movementSpeed;
+    public Camera playerCamera;
+    private Vector2 mousePosition;
+    private Vector3 desiredLocation;
+    private Rigidbody rb;
+    
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    { 
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void FixedUpdate()
+    {
+        if (transform.position != desiredLocation)
+        {
+            rb.MovePosition(Time.fixedDeltaTime * movementSpeed * desiredLocation.normalized);
+        }
+        else
+        {
+            desiredLocation = transform.position;
+        }
+    }
+
+    void OnMove()
+    {
+        Ray clickRay = playerCamera.ScreenPointToRay(mousePosition);
+        RaycastHit hit;
+        if (Physics.Raycast(clickRay, out hit))
+        {
+            Debug.DrawLine(playerCamera.transform.position, hit.point, Color.orange, 1);
+            desiredLocation = hit.point;
+        }
+    }
+
+    void OnLook(InputValue value)
+    {
+        mousePosition = value.Get<Vector2>();
+    }
+}
