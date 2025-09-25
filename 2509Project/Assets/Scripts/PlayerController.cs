d17e03f910fa64b3eda5f1395371488d777ae525
Wouter Meeder <woutermeeder67@gmail.com>
2025-09-25 10:25:58 +0200

    added skin for player

--- 2509Project/Assets/Scripts/PlayerController.cs
+++ 2509Project/Assets/Scripts/PlayerController.cs
@@ -9,18 +9,11 @@ public class PlayerController : MonoBehaviour
     private Vector3 desiredLocation;
     private Rigidbody rb;
     
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     { 
         rb = GetComponent<Rigidbody>();
     }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
+    
     void FixedUpdate()
     {
         if (transform.position != desiredLocation)
