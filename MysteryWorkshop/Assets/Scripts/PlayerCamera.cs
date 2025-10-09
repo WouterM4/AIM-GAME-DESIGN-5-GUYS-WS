ef18146622ee272cc41ed0df29ece21f1b516eca
Wouter Meeder <woutermeeder67@gmail.com>
2025-10-09 15:26:25 +0200

    camera fix

--- MysteryWorkshop/Assets/Scripts/PlayerCamera.cs
+++ MysteryWorkshop/Assets/Scripts/PlayerCamera.cs
@@ -5,17 +5,12 @@ using UnityEngine;
 // This script will be a NetworkBehaviour so that we can use the OnStartClient override.
 public class PlayerCamera : NetworkBehaviour
 {
-    [SerializeField] private GameObject prefab;
-    [SerializeField] private GameObject player;
-    [SerializeField] private Vector3 offset;
+    [SerializeField] private CinemachineCamera cinemachineCamera;
 
     // This method is called on the client after the object is spawned in.
     public override void OnStartClient()
     {
         // Simply enable our local cinemachine camera on the object if we are the owner.
-        cinemachineCamera = Instantiate(prefab, player.transform.position + offset, );
         cinemachineCamera.enabled = IsOwner;
-        
-        offset = cinemachineCamera.transform.position - player.transform.position;
     }
 }
